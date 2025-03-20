d32a4fc5668fcfa8a03306c017a49a7c7e5b9269
Maximka0304 <rolikpolik84@gmail.com>
2025-03-20 18:15:40 +0200

    Add files via upload

---
+++ lessons3now.py
@@ -0,0 +1,33 @@
+
+#Перше завдання
+#number = int(input("Напишіть число, яке б ви хотіли побачити в квадраті: "))
+#finish_number = number * number
+#print(f"Квадрат числа {number} дорівнює {finish_number}")
+#Друге завдання
+#print("Привіт,світ!")
+#Третє завдання
+#xp = int(input("Напиишіть число яке мені помножити?"))
+#xd = int(input("Напиишіть число яке мені помножити друге?"))
+#y = xp * xd
+#print("Дорівнює",y,)
+#Четверте завдання
+#def average(numbers):
+#  if not numbers:
+#    return 0  # Повертає 0, якщо список порожній
+#  total = sum(numbers)
+#  return total / len(numbers)
+#numbers = [1, 2, 3, 4, 5]
+#average_value = average(numbers)
+#print(f"Середнє арифметичне: {average_value}")
+#Пятте завдання
+#def pomnozh(x, n=2):
+#  return x ** n
+#Шосте завдання
+#def ploscha(shirina, visota):
+# return shirina * visota
+#Сьоме завдання
+#def bilsh(a, b):
+#  if a >= b:
+#    return a
+#  else:
+#    return b
